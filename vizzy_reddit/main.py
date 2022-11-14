7895420258d7f4965682d6b2c332e4bb2ace2849
Steve Haering - Ventera <111409615+maester-of-bots@users.noreply.github.com>
2022-11-13 20:01:55 -0500

    Update main.py

--- vizzy_reddit/main.py
+++ vizzy_reddit/main.py
@@ -407,7 +407,7 @@ class VIZZY_T:
                     self.response_sentient(redditObject, residual=True)
 
                 elif triggered:
-                    if redditObject.author.name.lower() == 'limacy':
+                    if redditObject.author.name.lower() in ['apostastrophe','limacy']:
                         self.response_sentient(redditObject)
                     else:
                         '''
